5c47468e8a263ad10f707ac1991c6ffe4036ea72
will-ireland <will_ireland@outlook.com>
2024-10-22 00:45:16 +1100

    Created bag_contents dict, able to save and delete bagItems from that dict interactively

--- bag/Bag.gd
+++ bag/Bag.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var selected_item
 var tilemap: TileMap
+var bag_contents = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 # adding in a placeholder item for testing
@@ -16,17 +17,22 @@ func _ready():
 	
 #placing item inside the bag using hardcoded vector
 	item.position = tilemap.map_to_local(Vector2i(1, 1))
+	
+	var cells = tilemap.get_used_cells(0)
+	for cell in cells:
+		bag_contents[cell] = null
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#attempting to save item to tilemap
+#saves item to bag_contents dict when pressing Enter, deletes when pressing Esc
 	if Input.is_action_just_pressed("ui_accept"):
-		var data = tilemap.get_cell_tile_data(0, selected_item.current_pos)
-		if data.get_custom_data("items") == null:
-			data.set_custom_data("items", selected_item)
+		if bag_contents[selected_item.current_pos] == null:
+			bag_contents[selected_item.current_pos] = selected_item
 		else: 
-			print(data.get_custom_data("items"))
+			print(bag_contents[selected_item.current_pos])
 	if Input.is_action_just_pressed("ui_cancel"):
-		var datas = tilemap.get_cell_tile_data(0, selected_item.current_pos)
-		print(datas.get_custom_data("items"))
+		print(bag_contents[selected_item.current_pos])
+		if bag_contents[selected_item.current_pos] != null:
+			bag_contents[selected_item.current_pos] = null
+		

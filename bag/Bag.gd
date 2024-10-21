2e334e5c6397e5d8069cde7e924a15ff258b4d85
will-ireland <will_ireland@outlook.com>
2024-10-22 00:28:57 +1100

    added comments, testing adding item to tilemap custom data layer

--- bag/Bag.gd
+++ bag/Bag.gd
@@ -1,23 +1,32 @@
 extends Node2D
 
-
+var selected_item
+var tilemap: TileMap
 # Called when the node enters the scene tree for the first time.
 func _ready():
 # adding in a placeholder item for testing
-	var tilemap = $TileMap
+	tilemap = $TileMap
+#instanciate item in scene - currently for testing purposes that an item is placed when starting scene
 	var scene = preload("res://items/BagItem.tscn")
+#instanciates an instance of an item and sends tilemap for calculations
 	var item = scene.instantiate()
 	item.set_world_coords(tilemap)
 	add_child(item)
-	
-	
+	selected_item = item
 	
 #placing item inside the bag using hardcoded vector
 	item.position = tilemap.map_to_local(Vector2i(1, 1))
-	#print(tilemap.get_surrounding_cells(Vector2i(1, 1)))
-	#item.position.x = 0
-	#item.position.y = 0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+#attempting to save item to tilemap
+	if Input.is_action_just_pressed("ui_accept"):
+		var data = tilemap.get_cell_tile_data(0, selected_item.current_pos)
+		if data.get_custom_data("items") == null:
+			data.set_custom_data("items", selected_item)
+		else: 
+			print(data.get_custom_data("items"))
+	if Input.is_action_just_pressed("ui_cancel"):
+		var datas = tilemap.get_cell_tile_data(0, selected_item.current_pos)
+		print(datas.get_custom_data("items"))
--- items/BagItem.gd
+++ items/BagItem.gd
@@ -13,9 +13,8 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
+#	motion is using a vector to represent which direction the next tile will be
 	var motion = Vector2i(0, 0)
-	
-# snap item to grid using hardcoded values, not connected to TileMap with logic yet
 	
 	#var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if Input.is_action_just_pressed("ui_down"):
@@ -31,17 +30,18 @@ func _physics_process(delta):
 		motion.x = SPEED
 		motion.y = 0
 		
+#	make sure there is room to move item before updating current_pos
 	if is_space_available(motion):
 		current_pos = motion + current_pos
 		position = tilemap.map_to_local(current_pos)
 		
 
+# receives tilemap to calculate whether an item is staying within confines of bag
 func set_world_coords(coords: TileMap):
 	tilemap = coords
 	
+# returns true only if tile exists in pocket layer in specified direction
 func is_space_available(motion: Vector2i):
-	print(motion + current_pos)
-	print(tilemap.get_cell_tile_data(0, motion + current_pos))
 	if (tilemap.get_cell_tile_data(0, motion + current_pos)):
 		return true
 	else:
